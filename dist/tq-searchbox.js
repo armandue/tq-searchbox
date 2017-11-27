66d3ea1caa6df3194724a6d8873ed8a4986ebff3
QingTian <qing.tian@targatelematics.com>
2017-11-27 12:16:52 +0100

    code style

--- dist/tq-searchbox.js
+++ dist/tq-searchbox.js
@@ -19,7 +19,7 @@ angular
 		}
 	});
 
-function TqSearchBoxController($window, $timeout, $filter) {
+function SearchBoxController($window, $timeout, $filter, $scope) {
 	var vm = this;
 	var oldOutput;
 	var focusedKey;
@@ -30,7 +30,7 @@ function TqSearchBoxController($window, $timeout, $filter) {
 	};
 
 	vm.$onInit = function () {
-    	vm.innerParameters = angular.copy(vm.parameters);
+		vm.innerParameters = angular.copy(vm.parameters);
 		angular.forEach(vm.startParameters, function (value, key) {
 			if (key === 'query') {
 				vm.output.query = value;
@@ -39,98 +39,98 @@ function TqSearchBoxController($window, $timeout, $filter) {
 				var indexOfOutputParameter = findFromArray({key: key}, vm.output.parameters);
 				var item = vm.innerParameters[indexOfParameter];
 				item.value = value;
-				if (indexOfOutputParameter !== -1) {
-					vm.output.parameters[indexOfOutputParameter] = item;
-				} else {
+				if (indexOfOutputParameter === -1) {
 					vm.output.parameters.push(item);
+				} else {
+					vm.output.parameters[indexOfOutputParameter] = item;
 				}
 				deleteFromArray(item, vm.innerParameters);
 			}
 		});
 
 		vm.start({resources: getOutputResources(vm.output)});
-    };
+	};
 
-    vm.$doCheck = function () {
-    	if (!angular.equals(oldOutput, vm.output)) {
-	    	vm.changeModel({resources: getOutputResources(vm.output)});
-    		oldOutput = angular.copy(vm.output);
-    	}
-    };
+	vm.$doCheck = function () {
+		if (!angular.equals(oldOutput, vm.output)) {
+			vm.changeModel({resources: getOutputResources(vm.output)});
+			oldOutput = angular.copy(vm.output);
+		}
+	};
 
-    vm.selectSearchItem = function (item, model, label, event) {
-    	vm.output.query = '';
-    	vm.output.parameters.push(item);
-    	focusOnItem(item);
-    	deleteFromArray(item, vm.innerParameters);
-    };
+	vm.selectSearchItem = function (item, model, label, event) {
+		vm.output.query = '';
+		vm.output.parameters.push(item);
+		focusOnItem(item);
+		deleteFromArray(item, vm.innerParameters);
+	};
 
-    vm.keydownOnParameter = function (event, parameter) {
-    	if (event.keyCode === 13) {
-			vm.checkInputValue(parameter)
-    	}
-    };
+	vm.keydownOnParameter = function (event, parameter) {
+		if (event.keyCode === 13) {
+			vm.checkInputValue(parameter);
+		}
+	};
 
-    vm.restoreSearch = function (parameter) {
-    	focusOnItem(parameter);
-    };
+	vm.restoreSearch = function (parameter) {
+		focusOnItem(parameter);
+	};
 
-    vm.removeParameter = function (parameter) {
-    	deleteFromArray(parameter, vm.output.parameters);
-    	addToArray(parameter, vm.innerParameters);
-    	vm.removeBox({resources: getOutputResources(vm.output)});
-    };
+	vm.removeParameter = function (parameter) {
+		deleteFromArray(parameter, vm.output.parameters);
+		addToArray(parameter, vm.innerParameters);
+		vm.removeBox({resources: getOutputResources(vm.output)});
+	};
 
-    vm.removeAllParameters = function () {
-    	vm.innerParameters = angular.copy(vm.parameters);
-    	vm.output = {
+	vm.removeAllParameters = function () {
+		vm.innerParameters = angular.copy(vm.parameters);
+		vm.output = {
 			query: '',
 			parameters: []
 		};
 		oldOutput = angular.copy(vm.output);
 		vm.removeAll();
-    };
+	};
 
-    vm.checkInputValue = function (parameter) {
+	vm.checkInputValue = function (parameter) {
 		focusedKey = '';
 		vm.leaveBox({resources: getOutputResources(vm.output)});
 		if (!parameter.value) {
 			vm.removeParameter(parameter);
 		}
-    };
+	};
 
-    vm.getSuggestions = function (parameter, viewValue) {
-    	if (parameter.dynamic) {
-    		return parameter.dynamicFunction(viewValue).then(function(response){
-              return response;
-            });
-    	} else {
-    		return $filter('filter')(parameter.suggestedValues, viewValue);
-    	}
-    };
+	vm.getSuggestions = function (parameter, viewValue) {
+		if (parameter.dynamic) {
+			return parameter.dynamicFunction(viewValue).then(function (response) {
+				return response;
+			});
+		} else {
+			return $filter('filter')(parameter.suggestedValues, viewValue);
+		}
+	};
 
-    vm.ifFocused = function (parameter) {
-    	return parameter.key === focusedKey;
-    };
+	vm.ifFocused = function (parameter) {
+		return parameter.key === focusedKey;
+	};
 
-    function focusOnItem (parameter) {
-    	focusedKey = parameter.key;
-		$timeout(function() {
+	function focusOnItem(parameter) {
+		focusedKey = parameter.key;
+		$timeout(function () {
 			var element = $window.document.getElementById(parameter.key);
-			if(element)
+			if (element) {
 				element.focus();
-			}, 100
-		);
+			}
+		}, 100);
 		vm.enterBox({resources: getOutputResources(vm.output)});
 	}
 
-	function getOutputResources (output) {
+	function getOutputResources(output) {
 		var parameterResources = {};
 		if (output.query) {
 			parameterResources.query = output.query;
 		}
 
-		angular.forEach(output.parameters, function(parameter) {
+		angular.forEach(output.parameters, function (parameter) {
 			parameterResources[parameter.key] = parameter.value;
 		});
 
@@ -138,18 +138,19 @@ function TqSearchBoxController($window, $timeout, $filter) {
 	}
 }
 
-function deleteFromArray (item, array) {
+function deleteFromArray(item, array) {
 	var index = findFromArray(item, array);
 	array.splice(index, 1);
 }
 
-function addToArray (item, array) {
+function addToArray(item, array) {
 	item.value = '';
 	array.push(item);
 }
 
-function findFromArray (item, array) {
-	var index, i = 0;
+function findFromArray(item, array) {
+	var index;
+	var i = 0;
 	angular.forEach(array, function (element) {
 		i++;
 		if (item.key === element.key) {
@@ -162,7 +163,6 @@ function findFromArray (item, array) {
 		return -1;
 	}
 }
-
 
 angular.module('tqSearchbox').run(['$templateCache', function($templateCache) {
   'use strict';
